b449987b6301074527202935efcdc28a8016658d
Andrew Chong <andrew.chong@digital.cabinet-office.gov.uk>
2015-04-24 12:20:43 +0100

    Functional tests for admin user through new DM

--- features/admin_journey.feature
+++ features/admin_journey.feature
@@ -5,22 +5,29 @@ Scenario: As an admin user I wish to log into Digital Marketplace, search for a 
   When I login as a 'Administrator' user
   Then I am presented with the 'Find a service' page
   When I enter '5126395806089216' in the 'Service ID' field
-  And I click the 'Find service' button
+  And I click 'Find service'
   Then I am presented with the summary page for that service
+
+Scenario: Admin user should be able to abort an edit and be returned to the service summary page
+  Given I am on the '/edit/description' page
+  When I click 'Return without saving'
+  Then I am presented with the summary page with no changes made to the 'Description'
 
 Scenario: As an admin user I wish to edit the description of a service
   Given I click the 'Edit' link for 'Description'
   Then I am presented with the 'edit' 'Description' page for that service
   When I change 'serviceName-text-box' to 'Service name changed'
   And I change 'serviceSummary-text-box' to 'Service summary changed'
-  And I click the 'Save and return to summary' button
+  And I click 'Save and return to summary'
   Then I am presented with the summary page with the changes that were made to the 'Description'
 
 Scenario: As an admin user I wish to edit the features and benefits of a service
   Given I click the 'Edit' link for 'Features and benefits'
   Then I am presented with the 'edit' 'Features and benefits' page for that service
-  When I change 'serviceFeatures' to 'Service feature changed'
-  And I click the 'Save and return to summary' button
+  When I change 'serviceFeatures-1' to 'Service feature changed'
+  And I remove 'serviceFeatures-2'
+  And I add 'New service benefit' as a 'serviceBenefits'
+  And I click 'Save and return to summary'
   Then I am presented with the summary page with the changes that were made to the 'Feature and benefits'
 
 Scenario: As an admin user I wish to edit the pricing of a service
@@ -28,23 +35,23 @@ Scenario: As an admin user I wish to edit the pricing of a service
   Then I am presented with the 'edit' 'Pricing' page for that service
   When I change 'priceMin' to '100'
   And I change 'priceMax' to '1234'
-  And I change 'priceUnit' to 'Unit'
-  And I change 'priceInterval' to 'Hour'
-  And I click the 'Save and return to summary' button
+  And I set 'priceUnit' as 'Person'
+  And I set 'priceInterval' as 'Week'
+  And I choose 'No' for 'vatIncluded'
+  And I choose 'No' for 'educationPricing'
+  And I choose 'Yes' for 'trialOption'
+  And I choose 'No' for 'freeOption'
+  And I choose 'Other' for 'minimumContractPeriod'
+  And I click 'Save and return to summary'
   Then I am presented with the summary page with the changes that were made to the 'Pricing'
 
 Scenario: As an admin user I wish to change a document of a service
   Given I click the 'Edit' link for 'Documents'
   Then I am presented with the 'edit' 'Documents' page for that service
   When I change 'pricingDocumentURL' file to '12345-test-new-pricing-document.pdf'
-  And I click the 'Save and return to summary' button
+  And I click 'Save and return to summary'
   Then I am presented with the summary page with the changes that were made to the 'Documents'
 
-Scenario: Admin user should be able to abort an edit and be returned to the service summary page
-  Given I am on the '/edit/description' page
-  When I click the 'Return without saving' link
-  Then I am presented with the summary page with no changes made to the 'Description'
-
 Scenario: Admin user is able to view service details page for a service
-  When I click the 'View service' link
+  When I click 'View service'
   Then I am presented with the service details page for that service
--- features/step_definitions/journey_test_steps.rb
+++ features/step_definitions/journey_test_steps.rb
@@ -2,3 +2,315 @@ require "ostruct"
 require "rest_client"
 
 store = OpenStruct.new
+store.changed_fields = store.changed_fields || Hash.new
+
+Given /I am on the '(.*)' login page$/ do |user_type|
+  if user_type == 'Administrator'
+    visit("#{dm_admin_frontend_domain}/admin/login")
+    page.should have_content("#{user_type}" ' login')
+    page.should have_content('Username')
+    page.should have_content('Password')
+  else user_type == 'Suppliers'
+    visit("#{dm_supplier_frontend_domain}/#{user_type.downcase}/login")
+    page.should have_content('Email address')
+    page.should have_content('Passphrase')
+    page.should have_content('Login')
+  end
+end
+
+When /I login as a '(.*)' user$/ do |user_type|
+  if user_type == 'Administrator'
+    fill_in('username', :with => ENV['DM_ADMINISTRATORNAME'])
+    fill_in('password', :with => ENV['DM_ADMINISTRATORPASSWORD'])
+    click_link_or_button('Log in')
+  else user_type == 'Supplier'
+    fill_in('email-address', :with => ENV['DM_SUPPLIEREMAIL'])
+    fill_in('passphrase', :with => ENV['DM_SUPPLIERPASSPHRASE'])
+    click_link_or_button('Login')
+    page.should have_content('Dashboard')
+  end
+end
+
+Then /I am presented with the '(.*)' page$/ do |page_name|
+  page.should have_content(page_name)
+  page.should have_content('Service ID')
+end
+
+When /I enter '(.*)' in the '(.*)' field$/ do |value,field_name|
+  fill_in(field_name, with: value)
+  store.serviceID = value
+end
+
+And /I click the '(.*)' button$/ do |button_name|
+  click_link_or_button(button_name)
+end
+
+Then /I am presented with the summary page for that service$/ do
+  store.existing_values = store.existing_values || Hash.new
+  store.existing_values['summarypageurl'] = current_url
+
+  current_url.should have_content(store.serviceID)
+  page.should have_content('Service attributes')
+  page.should have_content('Description')
+  page.should have_content('Features and benefits')
+  page.should have_content('Pricing')
+  page.should have_content('Documents')
+
+  servicename = find(
+    :xpath,
+    "//table[2]/tbody/tr[1]/td[2]"
+  ).text()
+  store.existing_values['servicename'] = servicename
+
+  servicesummary = find(
+    :xpath,
+    "//table[2]/tbody/tr[2]/td[2]"
+  ).text()
+  store.existing_values['servicesummary'] = servicesummary
+
+  if store.serviceID.include?('-G')
+    store.existing_values['servicefeature1'] = ''
+    store.existing_values['servicebenefits1'] = ''
+  else
+    servicefeature1 = find(
+      :xpath,
+      "//table[3]/tbody/tr[1]/td[2]/ul/li[1]"
+    ).text()
+    store.existing_values['servicefeature1'] = servicefeature1
+
+    servicebenefits1 = find(
+      :xpath,
+      "//table[3]/tbody/tr[2]/td[2]/ul/li[1]"
+    ).text()
+    store.existing_values['servicebenefits1'] = servicebenefits1
+  end
+
+  serviceprice = find(
+    :xpath,
+    "//table[4]/tbody/tr[1]/td[2]"
+  ).text()
+  store.existing_values['serviceprice'] = serviceprice
+
+  vatincluded = find(
+    :xpath,
+    "//table[4]/tbody/tr[2]/td[2]"
+  ).text()
+  store.existing_values['vatincluded'] = vatincluded
+
+  educationpricing = find(
+    :xpath,
+    "//table[4]/tbody/tr[3]/td[2]"
+  ).text()
+  store.existing_values['educationpricing'] = educationpricing
+
+  trialoption = find(
+    :xpath,
+    "//table[4]/tbody/tr[4]/td[2]"
+  ).text()
+  store.existing_values['trialoption'] = trialoption
+
+  freeoption = find(
+    :xpath,
+    "//table[4]/tbody/tr[5]/td[2]"
+  ).text()
+  store.existing_values['freeoption'] = freeoption
+
+  minimumcontractperiod = find(
+    :xpath,
+    "//table[4]/tbody/tr[6]/td[2]"
+  ).text()
+  store.existing_values['minimumcontractperiod'] = minimumcontractperiod
+
+  pricingdocument = find(
+    :xpath,
+    "//table[5]/tbody/tr[4]/td[2]/a[@href]"
+  ).text()
+  store.existing_values['pricingdocument'] = pricingdocument
+end
+
+Given /I click the '(.*)' link for '(.*)'$/ do |action,service_aspect|
+  find(
+    :xpath,
+    "//a[@href = '/admin/service/#{store.serviceID.downcase}/#{action.downcase}/#{service_aspect.gsub(' ','_').downcase}']"
+  ).click
+end
+
+Then /I am presented with the '(.*)' '(.*)' page for that service$/ do |action,service_aspect|
+  current_url.should end_with("#{store.existing_values['summarypageurl']}/#{action.downcase}/#{service_aspect.gsub(' ','_').downcase}")
+
+  if service_aspect == 'Description'
+    page.should have_content(service_aspect)
+    page.should have_content('Service name')
+    page.should have_content('Service summary')
+  elsif service_aspect == 'Features and benefits'
+    page.should have_content(service_aspect)
+    page.should have_content('Service features')
+    page.should have_content('Service benefits')
+  elsif service_aspect == 'Pricing'
+    page.should have_content(service_aspect)
+    page.should have_content('Service price')
+    page.should have_content('VAT included')
+    page.should have_content('Education pricing')
+    page.should have_content('Trial option')
+    page.should have_content('Free option')
+    page.should have_content('Minimum contract period')
+  else service_aspect == 'Documents'
+    page.should have_content(service_aspect)
+    page.should have_content('Service definition document')
+    page.should have_content('Please upload your terms and conditions document')
+    page.should have_content('Skills Framework for the Information Age (SFIA) rate card')
+    page.should have_content('Pricing document')
+  end
+end
+
+When /I change '(.*)' to '(.*)'$/ do |field_to_change,new_value|
+  if page.has_content?('Features and benefits')
+    find(
+      :xpath,
+      "//*[contains(@id, '#{field_to_change.split('-').last}') and contains(@name, '#{field_to_change.split('-').first}')]"
+    ).set(new_value)
+  else
+    find(
+      :xpath,
+      "//*[contains(@id, '#{field_to_change}')]"
+    ).set(new_value)
+  end
+
+  store.changed_fields[field_to_change] = new_value
+end
+
+And /I set '(.*)' as '(.*)'$/ do |field_to_change,new_value|
+  within "##{field_to_change}" do
+    page.find("option[value=#{new_value}]").select_option
+  end
+  store.changed_fields[field_to_change] = new_value.downcase
+end
+
+And /I choose '(.*)' for '(.*)'$/ do |new_value,field_to_change|
+  if field_to_change == 'minimumContractPeriod'
+    find(
+      :xpath,
+      "//*[contains(@name, '#{field_to_change}') and contains(@value, '#{new_value}')]"
+    ).click
+  else
+    choose("#{field_to_change}-#{new_value.downcase}")
+  end
+  store.changed_fields[field_to_change] = new_value
+end
+
+And /I remove '(.*)'$/ do |item_to_remove|
+  store.changed_fields[item_to_remove] = find(
+    :xpath,
+    "//*[contains(@id, '#{item_to_remove}')]"
+  ).value()
+  click_link_or_button('service feature number 2', visible: false)
+end
+
+And /I add '(.*)' as a '(.*)'$/ do |value,item_to_add|
+  record_number_to_add = (10 - ((find(
+    :xpath,
+    "//button[contains(text(), 'Add another service benefit')]"
+  ).text()).split('Add another service benefit (').last.split(' remaining').first).to_i)
+
+  find(
+    :xpath,
+    "//button[contains(text(), 'Add another service benefit')]"
+  ).click
+
+  find(
+    :xpath,
+    "//*[contains(@id, '#{record_number_to_add}') and contains(@name, '#{item_to_add}')]"
+  ).set(value)
+
+  store.changed_fields[item_to_add] = value
+end
+
+Then /I am presented with the summary page with the changes that were made to the '(.*)'$/ do |service_aspect|
+  current_url.should end_with(store.existing_values['summarypageurl'])
+  if service_aspect == 'Description'
+    page.should have_content(store.changed_fields['serviceName-text-box'])
+    page.should have_content(store.changed_fields['serviceSummary-text-box'])
+  elsif service_aspect == 'Features and benefits'
+    page.should have_content(store.changed_fields['serviceFeatures-1'])
+    page.should have_no_content(store.changed_fields['serviceFeatures-2'])
+    page.should have_content(store.changed_fields['serviceBenefits'])
+  elsif service_aspect == 'Pricing'
+    find(
+      :xpath,
+      "//*[contains(text(), 'Service price')]/following-sibling::*[1]"
+    ).text().should have_content("£#{store.changed_fields['priceMin']} to £#{store.changed_fields['priceMax']} per #{store.changed_fields['priceUnit']} per #{store.changed_fields['priceInterval']}")
+    find(
+      :xpath,
+      "//*[contains(text(), 'VAT included')]/following-sibling::*[1]"
+    ).text().should have_content(store.changed_fields['vatIncluded'])
+    find(
+      :xpath,
+      "//*[contains(text(), 'Education pricing')]/following-sibling::*[1]"
+    ).text().should have_content(store.changed_fields['educationPricing'])
+    find(
+      :xpath,
+      "//*[contains(text(), 'Trial option')]/following-sibling::*[1]"
+    ).text().should have_content(store.changed_fields['trialOption'])
+    find(
+      :xpath,
+      "//*[contains(text(), 'Free option')]/following-sibling::*[1]"
+    ).text().should have_content(store.changed_fields['freeOption'])
+    find(
+      :xpath,
+      "//*[contains(text(), 'Minimum contract period')]/following-sibling::*[1]"
+    ).text().should have_content(store.changed_fields['minimumContractPeriod'])
+  elsif service_aspect == 'Documents'
+    page.should have_no_content(store.existing_values['pricingdocument'])
+    store.newpricingdocument = find(
+      :xpath,
+      "//table[5]/tbody/tr[4]/td[2]/a[@href]"
+    ).text()
+
+    if store.newpricingdocument == store.existing_values['pricingdocument']
+      raise "The pricing document has not been changed as expected"
+    end
+  end
+end
+
+When /I change '(.*)' file to '(.*)'$/ do |document_to_change,new_document|
+  find(
+    :xpath,
+    "//*[contains(@name, '#{document_to_change}')]"
+  ).click
+  attach_file(document_to_change, File.join(Dir.pwd, 'fixtures', new_document))
+end
+
+When /I click '(.*)'$/ do |button_link_name|
+  page.click_link_or_button(button_link_name)
+end
+
+Given /I am on the '(.*)' page$/ do |page_to_visit|
+  visit("#{store.existing_values['summarypageurl']}#{page_to_visit}")
+  current_url.should end_with("#{store.existing_values['summarypageurl']}#{page_to_visit}")
+end
+
+Then /I am presented with the summary page with no changes made to the '(.*)'$/ do |service_aspect|
+  current_url.should end_with(store.existing_values['summarypageurl'])
+  page.should have_content(store.existing_values['servicename'])
+  page.should have_content(store.existing_values['servicesummary'])
+  page.should have_content(store.existing_values['servicefeature1'])
+  page.should have_content(store.existing_values['servicebenefits1'])
+  page.should have_content(store.existing_values['serviceprice'])
+  page.should have_content(store.existing_values['vatincluded'])
+  page.should have_content(store.existing_values['educationpricing'])
+  page.should have_content(store.existing_values['trialoption'])
+  page.should have_content(store.existing_values['freeoption'])
+  page.should have_content(store.existing_values['minimumcontractperiod'])
+  page.should have_content(store.existing_values['pricingdocument'])
+end
+
+#Visit service link currently not set to go to correct place.
+Then /I am presented with the service details page for that service$/ do
+  visit("#{dm_admin_frontend_domain}/service/#{store.serviceID}")
+  current_url.should end_with("#{dm_admin_frontend_domain}/service/#{store.serviceID}")
+  page.should have_content(store.changed_fields['serviceName-text-box'])
+  page.should have_content(store.changed_fields['serviceSummary-text-box'])
+  page.should have_content(store.changed_fields['serviceFeatures-1'])
+  page.should have_no_content(store.changed_fields['serviceFeatures-2'])
+  page.should have_content("£#{store.changed_fields['priceMin']} to £#{store.changed_fields['priceMax']} per #{store.changed_fields['priceUnit']} per #{store.changed_fields['priceInterval']}")
+end
