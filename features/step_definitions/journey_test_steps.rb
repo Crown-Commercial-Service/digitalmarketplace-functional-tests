46b45f2b54611fd71ed56af15edf2659413dbfbe
Andrew Chong <andrew.chong@digital.cabinet-office.gov.uk>
2015-07-29 11:45:21 +0100

    removed time delay and moved check of url to be after content on page check

--- features/step_definitions/journey_test_steps.rb
+++ features/step_definitions/journey_test_steps.rb
@@ -381,8 +381,6 @@ Then /I am presented with the dashboard page with the changes that were made to 
 end
 
 Then /I am presented with the summary page with the changes that were made to the '(.*)'$/ do |service_aspect|
-  sleep 1
-  current_url.should end_with(@existing_values['summarypageurl'])
   if service_aspect == 'Description'
     page.should have_content(@changed_fields['serviceName'])
     page.should have_content(@changed_fields['serviceSummary'])
@@ -431,6 +429,7 @@ Then /I am presented with the summary page with the changes that were made to th
       raise "The pricing document has not been changed as expected"
     end
   end
+  current_url.should end_with(@existing_values['summarypageurl'])
 end
 
 When /I change '(.*)' file to '(.*)'$/ do |document_to_change,new_document|
