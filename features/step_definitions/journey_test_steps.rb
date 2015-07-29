54cf128f5ca40bfbf9b103b5630beab22d758618
Andrew Chong <andrew.chong@digital.cabinet-office.gov.uk>
2015-07-29 10:43:02 +0100

    add time delay in attempt to fix intermittent preview functional test failure

--- features/step_definitions/journey_test_steps.rb
+++ features/step_definitions/journey_test_steps.rb
@@ -381,6 +381,7 @@ Then /I am presented with the dashboard page with the changes that were made to 
 end
 
 Then /I am presented with the summary page with the changes that were made to the '(.*)'$/ do |service_aspect|
+  sleep 1
   current_url.should end_with(@existing_values['summarypageurl'])
   if service_aspect == 'Description'
     page.should have_content(@changed_fields['serviceName'])
