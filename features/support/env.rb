4673e022879eb6343e6f0ff6f47451a20ef3bfaa
Paul Craig <paul.craig@digital.cabinet-office.gov.uk>
2015-09-30 16:37:20 +0100

    Silence JS console.log messages.
    One of [Tom's pull requests](https://github.com/alphagov/digitalmarketplace-frontend-toolkit/pull/164)
    put in a bunch of JavaScript for module loading, which were pretty
    annoying when running the functional tests locally.
    Found a useful gist showing how to suppress console messages in Poltergiest.
    https://gist.github.com/benjaminheng/d52222e298eab09af23d

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,7 +16,8 @@ else
   Capybara.default_driver = :poltergeist
 
   Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, timeout: 180, :phantomjs_options => ['--ssl-protocol=TLSv1', '--ignore-ssl-errors=yes', '--local-to-remote-url-access=yes'])
+    Capybara::Poltergeist::Driver.new(
+      app, :timeout => 180, :phantomjs_logger => File.open(File::NULL, "w"), :phantomjs_options => ['--ssl-protocol=TLSv1', '--ignore-ssl-errors=yes', '--local-to-remote-url-access=yes'])
   end
 end
 
