40c9a539ea4c779241cd56e5ff14b227bf3ec5ba
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2016-01-27 10:59:44 +0000

    Make Nginx bootstrapping more robust
    Only do actions if required to avoid sudo when not needed.
    These changes mean that it's less disruptive to put this on the normal
    test execution path.
    
    - Check if nginx is not installed before trying to install.
    - Check if installed nginx config is different before installing.
    - Only reload the nginx config if it has changed.

--- nginx/bootstrap.sh
+++ nginx/bootstrap.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
-brew install nginx
-sudo cp "$(dirname "${BASH_SOURCE[0]}")/nginx.conf" /usr/local/etc/nginx/nginx.conf
+if [ "$(brew ls --versions nginx | wc -l)" -eq 0 ]; then
+  brew install nginx
+fi
 
-if sudo ps -xU nobody > /dev/null 2>&1; then
-  echo "Nginx running, reloading config"
-  sudo nginx -s reload
+local_config="$(dirname "$BASH_SOURCE[0]}")/nginx.conf"
+installed_config="/usr/local/etc/nginx/nginx.conf"
+config_updated=0
+if [ "$(diff "$local_config" "$installed_config" | wc -l)" -gt 0 ]; then
+  echo "Installing updated Nginx config"
+  sudo cp "$local_config" "$installed_config"
+  config_updated=1
+fi
+
+if ps -xU nobody > /dev/null 2>&1; then
+  if [ "$config_updated" -eq 1 ]; then
+    echo "Nginx running, reloading config"
+    sudo nginx -s reload
+  fi
 else
   echo "Starting Nginx"
   sudo nginx
