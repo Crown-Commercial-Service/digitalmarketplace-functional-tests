456f97715ebcd1c16fee95019870a41e8ae0e17c
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-02-06 15:31:22 +0000

    Extend api helpers - creation of briefs/suppliers
    Useful functions are added to the api helpers which can be reused for
    creating briefs, suppliers and services.

--- features/support/api_helpers.rb
+++ features/support/api_helpers.rb
@@ -6,9 +6,9 @@ def call_api(method, path, options={})
   url = "#{domain}#{path}"
   payload = options.delete(:payload)
   options.merge!({
-    :content_type => :json,
-    :accept => :json,
-    :authorization => "Bearer #{auth_token}"
+    content_type: :json,
+    accept: :json,
+    authorization: "Bearer #{auth_token}"
   })
   if payload.nil?
     RestClient.send(method, url, options) {|response, request, result| response}
@@ -32,8 +32,8 @@ def update_framework_status(framework_slug, status)
   framework = JSON.parse(response.body)["frameworks"]
   if framework['status'] != status
     response = call_api(:post, "/frameworks/#{framework_slug}", payload: {
-      "frameworks" => {"status" => status, "clarificationQuestionsOpen" => status == 'open'},
-      "updated_by" => "functional tests",
+      frameworks: {status: status, clarificationQuestionsOpen: status == 'open'},
+      updated_by: "functional tests",
     })
     response.code.should be(200), _error(response, "Failed to update framework status #{framework_slug} #{status}")
   end
@@ -50,15 +50,15 @@ end
 
 def ensure_user_exists(user_details)
   creation_response = call_api(:post, "/users", payload: {
-    "users" => user_details,
-    "updated_by" => "functional tests",
+    users: user_details,
+    updated_by: "functional tests",
   })
   if creation_response.code == 409
     # user with this email already exists - let's see if we have the right password...
     auth_response = call_api(:post, "/users/auth", payload: {
-      "authUsers" => {
-        "emailAddress" => user_details['emailAddress'],
-        "password" => user_details['password'],
+      authUsers: {
+        emailAddress: user_details['emailAddress'],
+        password: user_details['password'],
       },
     })
     unless auth_response.code == 200
@@ -67,8 +67,8 @@ def ensure_user_exists(user_details)
       # need to discover the user id to target...
       user = get_user_by_email(user_details['emailAddress'])
       reset_failed_login_response = call_api(:post, "/users/#{user['id']}", payload: {
-        "users" => {"locked" => false},
-        "updated_by" => "functional tests",
+        users: {locked: false},
+        updated_by: "functional tests",
       })
       reset_failed_login_response.code.should be(200), _error(reset_failed_login_response, "Failed to ensure user #{user_details['emailAddress']} exists")
       # this should definitely fail now
@@ -91,8 +91,8 @@ end
 
 def update_framework_agreement_status(framework_slug, supplier_id, status)
   response = call_api(:post, "/suppliers/#{supplier_id}/frameworks/#{framework_slug}", payload: {
-    "frameworkInterest" => {"agreementReturned" => status},
-    "updated_by" => "functional tests",
+    frameworkInterest: {agreementReturned: status},
+    updated_by: "functional tests",
   })
   response.code.should be(200), _error(response, "Failed to update agreement status #{supplier_id} #{framework_slug}")
 end
@@ -102,7 +102,7 @@ def register_interest_in_framework(framework_slug, supplier_id)
   response = call_api(:get, path)
   if response.code == 404
     response = call_api(:put, path, payload: {
-      "updated_by" => "functional tests"
+      updated_by: "functional tests"
     })
     response.code.should match(/20[01]/), _error(response, "Failed to register interest in framework #{framework_slug} #{supplier_id}")
   end
@@ -111,8 +111,90 @@ end
 def submit_supplier_declaration(framework_slug, supplier_id, declaration)
   path = "/suppliers/#{supplier_id}/frameworks/#{framework_slug}/declaration"
   response = call_api(:put, path, payload: {
-    "declaration" => declaration,
-    "updated_by" => "functional tests",
+    declaration: declaration,
+    updated_by: "functional tests",
   })
   [200, 201].should include(response.code), _error(response, "Failed to submit supplier declaration #{framework_slug} #{supplier_id}")
+  JSON.parse(response.body)['declaration']
+end
+
+def create_brief(lot_slug, user_id)
+  brief_data = {
+    updated_by: "functional tests"
+  }
+  case lot_slug
+  when 'digital-specialists'
+    brief_data['briefs'] = Fixtures::DIGITAL_SPECIALISTS_BRIEF
+  when 'digital-outcomes'
+    brief_data['briefs'] = Fixtures::DIGITAL_OUTCOMES_BRIEF
+  when 'user-research-participants'
+    brief_data['briefs'] = Fixtures::USER_RESEARCH_PARTICIPANTS_BRIEF
+  else
+    puts 'Lot slug not recognised'
+  end
+
+  brief_data['briefs']['userId'] = user_id
+
+  response = call_api(:post, '/briefs', payload: brief_data)
+  response.code.should be(201), _error(response, "Failed to create brief for #{lot_slug}, #{user_id}")
+  JSON.parse(response.body)['briefs']['id']
+end
+
+def publish_brief(brief_id)
+  path = "/briefs/#{brief_id}/publish"
+  response = call_api(:post, path, payload: {
+    updated_by: "functional tests"
+  })
+  response.code.should be(200), _error(response, "Failed to publish brief #{brief_id}")
+end
+
+def create_supplier
+  random_string = SecureRandom.hex
+
+  response = call_api(:post, "/suppliers", payload: {
+    updated_by: "functional tests",
+    suppliers: {
+      name: 'functional test supplier',
+      dunsNumber: rand(9999999999).to_s,
+      contactInformation: [
+        {
+          contactName: random_string,
+          email: random_string + "-supplier@user.dmdev",
+          phoneNumber: '%010d' % rand(10 ** 11 -1),
+        }
+      ]
+    }
+  })
+  response.code.should be(201), _error(response, "Failed to create supplier")
+  JSON.parse(response.body)['suppliers']
+end
+
+def create_live_service(lot_slug, supplier_id)
+  # Create a 15 digit service ID, miniscule clash risk
+  start = 10 ** 14
+  last = 10 ** 15 - 1
+  random_service_id = rand(start..last).to_s
+
+  service_data = {
+    updated_by: 'functional_tests',
+  }
+
+  case lot_slug
+    when 'digital-specialists'
+      service_data['services'] = Fixtures::DIGITAL_SPECIALISTS_SERVICE
+    when 'digital-outcomes'
+      service_data['services'] = Fixtures::DIGITAL_OUTCOMES_SERVICE
+    when 'user-research-participants'
+      service_data['services'] = Fixtures::USER_RESEARCH_PARTICIPANTS_SERVICE
+    else
+      puts 'Lot slug not recoginsed'
+  end
+
+  service_data['services']['id'] = random_service_id
+  service_data['services']['supplierId'] = supplier_id
+
+  service_path = "/services/#{random_service_id}"
+  response = call_api(:put, service_path, payload: service_data)
+  response.code.should be(201), response.body
+  JSON.parse(response.body)['services']
 end
