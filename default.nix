2a8ad6077a72b7d80d639c44f4c45d6c4a17bc6f
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2017-07-05 16:07:20 +0100

    default.nix: add mechanism for making local modifications to nix environment

--- default.nix
+++ default.nix
@@ -1,8 +1,13 @@
-{
-  pkgs ? import <nixpkgs> {}
-}:
-{
-  digitalMarketplaceFunctionalTestsEnv = (pkgs.bundlerEnv {
+argsOuter@{...}:
+let
+  # specifying args defaults in this slightly non-standard way to allow us to include the default values in `args`
+  args = rec {
+    pkgs = import <nixpkgs> {};
+    localOverridesPath = ./local.nix;
+  } // argsOuter;
+in (with args; {
+  digitalMarketplaceFunctionalTestsEnv = (
+    (pkgs.bundlerEnv {
       name = "digitalmarketplace-functional-tests-bundler-env";
 
       ruby = pkgs.ruby;
@@ -10,15 +15,16 @@
       lockfile = ./Gemfile.lock;
       gemset = ./gemset.nix;
     }).env.overrideAttrs (oldAttrs: oldAttrs // {
-    name = "digitalmarketplace-functional-tests-env";
-    buildInputs = [
-      pkgs.bundler
-      pkgs.bundix
-      pkgs.libxml2
-      pkgs.phantomjs
-    ];
+      name = "digitalmarketplace-functional-tests-env";
+      buildInputs = [
+        pkgs.bundler
+        pkgs.bundix
+        pkgs.libxml2
+        pkgs.phantomjs
+      ];
 
-    # if we don't have this, we get unicode troubles in a --pure nix-shell
-    LANG="en_GB.UTF-8";
-  });
-}
+      # if we don't have this, we get unicode troubles in a --pure nix-shell
+      LANG="en_GB.UTF-8";
+    })
+  ).overrideAttrs (if builtins.pathExists localOverridesPath then (import localOverridesPath args) else (x: x));
+})
