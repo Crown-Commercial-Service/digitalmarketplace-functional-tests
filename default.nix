ecef3c4fcc079dffd0aa65cf425b767e9e2dc6fb
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2017-07-20 18:25:12 +0100

    default.nix: make $PS1 colors more visible

--- default.nix
+++ default.nix
@@ -28,7 +28,7 @@ in (with args; {
       LANG="en_GB.UTF-8";
 
       shellHook = ''
-        export PS1="\[\e[0;34m\](nix-shell\[\e[0m\]:\[\e[0;34m\]${shortName})\[\e[0;32m\]\u@\h\[\e[0m\]:\[\e[0m\]\[\e[0;34m\]\w\[\e[0m\]\$ "
+        export PS1="\[\e[0;36m\](nix-shell\[\e[0m\]:\[\e[0;36m\]${shortName})\[\e[0;32m\]\u@\h\[\e[0m\]:\[\e[0m\]\[\e[0;36m\]\w\[\e[0m\]\$ "
       '';
     })
   ).overrideAttrs (if builtins.pathExists localOverridesPath then (import localOverridesPath args) else (x: x));
