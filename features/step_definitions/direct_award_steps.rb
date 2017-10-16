1fa32cc243addf994fa262e4f80fa8b519f8b2ca
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2017-10-16 11:16:01 +0100

    Fix direct award content from recent buyer-fe PR

--- features/step_definitions/direct_award_steps.rb
+++ features/step_definitions/direct_award_steps.rb
@@ -2,7 +2,7 @@ When(/^I have created and saved a search$/) do
   steps %Q{
     Given I am on the /g-cloud/search page
     And I click 'Save search'
-    Then I am on the 'Save your search' page
+    Then I am on the 'Choose where to save your search' page
     And I enter 'my cloud project' in the 'Name your search' field
     And I click 'Save and continue'
   }
