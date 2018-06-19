cd47ae5e69639e70b5f0fd61f5e5925281a4ba0a
Andrew Chong <andrew.chong@digital.cabinet-office.gov.uk>
2018-06-19 11:55:02 +0100

    User awards contract runs but failing on unimplemented step <Then I see "You've updated 'my cloud project'">

--- features/buyer/direct-award.feature
+++ features/buyer/direct-award.feature
@@ -83,12 +83,15 @@ Scenario: User ends search and downloads results
   And I click the 'Download search results as comma-separated values' link
   And I should get a download file of type 'csv'
 
-
+@direct-award-new
 Scenario: User awards contract
   Given I am logged in as a buyer user
-  When I have created and ended a search called 'my cloud project'
-  And I click on the 'Tell us your outcome' link
-  And I award the contract to 'Supplier X'
+  And I have created and ended a search called 'my cloud project'
+  And I have downloaded the search results as a file of type 'ods'
+  And I click the 'Return to overview' link
+  And I am on the 'my cloud project' page
+  When I click the 'Tell us the outcome' link
+  And I award the contract to 'NCCIS' for the 'my cloud project' search
   And I am on the 'my cloud project' page
   Then I see "You've updated 'my cloud project'"
   And I see 'Contract awarded to Supplier X: Service Y'
@@ -96,7 +99,7 @@ Scenario: User awards contract
 Scenario: User does not award contract as work is cancelled
   Given I am logged in as a buyer user
   When I have created and ended a search called 'my cloud project'
-  And I click on the 'Tell us your outcome' link
+  And I click the 'Tell us the outcome' link
   And I do not award the contract because the work is cancelled
   And I am on the 'my cloud project' page
   Then I see "You've updated 'my cloud project'"
@@ -105,7 +108,7 @@ Scenario: User does not award contract as work is cancelled
 Scenario: User does not award contract as there are no suitable services
   Given I am logged in as a buyer user
   When I have created and ended a search called 'my cloud project'
-  And I click on the 'Tell us your outcome' link
+  And I click the 'Tell us the outcome' link
   And I do not award the contract because there are no suitable services
   And I am on the 'my cloud project' page
   Then I see "You've updated 'my cloud project'"
@@ -114,7 +117,7 @@ Scenario: User does not award contract as there are no suitable services
 Scenario: User is still assessing services
   Given I am logged in as a buyer user
   When I have created and ended a search called 'my cloud project'
-  And I click on the 'Tell us your outcome' link
+  And I click the 'Tell us the outcome' link
   And I choose the 'Still assessing' radio button
   And I click "Save and Continue"
   And I am on the 'my cloud project' page
--- features/step_definitions/direct_award_steps.rb
+++ features/step_definitions/direct_award_steps.rb
@@ -8,19 +8,46 @@ When(/^I have created and saved a search called '(.*)'$/) do |search_name|
   }
 end
 
-When(/^I award the contract to '(.*)'$/) do |supplier_name|
+When (/^I have created and ended a search called '(.*)'$/) do |search_name|
   steps %{
-    Given I am on the 'Did you award contract?' page
+    And I have created and saved a search called '#{search_name}'
+    And I am on the /buyers page
+    Then I click the 'View your saved searches' link
+    Then I click the '#{search_name}' link
+    Then I am on the '#{search_name}' page
+    And I click the 'End search' link
+    Then I am on the 'End your search' page
+    And I click the 'End search and continue' button
+    Then I am on the '#{search_name}' page
+  }
+end
+
+When (/^I have downloaded the search results as a file of type '(.*)'$/) do |file_type|
+  steps %{
+    And I click the 'Download search results' link
+    And I am on the 'Download your search results' page
+    And I click the 'Download search results as a spreadsheet' link
+    And I should get a download file of type '#{file_type}'
+  }
+end
+
+When (/^I award the contract to '(.*)' for the '(.*)' search$/) do |supplier_name, search_name|
+  steps %{
+    Given I am on the 'Did you award a contract for ‘#{search_name}’?' page
     And I choose the 'Yes' radio button
     And I click 'Save and continue'
-    The I am on the 'Which service did you award?' page
-    And I choose the 'Service X' radio button
+    Then I am on the 'Which service won the contract?' page
+    And I choose the '#{supplier_name}' radio button
     And I click 'Save and continue'
     Then I am on the 'Tell us about your contract' page
-    And I fill in start date as '20-12-2018'
-    And I fill in end date as '20-12-2020'
-    And I fill in the value as '100000'
-    And I fill in the organisation buying the service as 'Government Digital Service'
+    And I enter '20' in the 'input-start_date-day' field
+    And I enter '12' in the 'input-start_date-month' field
+    And I enter '2018' in the 'input-start_date-year' field
+    And I enter '20' in the 'input-end_date-day' field
+    And I enter '12' in the 'input-end_date-month' field
+    And I enter '2020' in the 'input-end_date-year' field
+    And I enter '100000' in the 'input-value_in_pounds' field
+    And I enter 'Government Digital Service' in the 'input-buying_organisation' field
     And I click 'Submit'
   }
 end
