4ab787d695ef289b0b961d888e55feebdccd762f
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-06-18 12:59:15 +0100

    nasty debug log: this should be in the reports/ dir if we want to keep it

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -10,7 +10,9 @@ Before do
   Capybara.reset_sessions!
 end
 
-DEBUG_FILE = File.open("debug.out", "w+")
+require 'fileutils'
+FileUtils.mkdir_p "reports"
+DEBUG_FILE = File.open("reports/debug.out", "w+")
 
 AfterStep do |result, test_step|
   DEBUG_FILE.write "#{Time.now.getutc}: #{test_step.location}\n"
