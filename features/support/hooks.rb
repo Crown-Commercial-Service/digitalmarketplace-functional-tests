4ca496fb4dd1b873dddd4dc007a7b3cff96da853
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-06-19 12:28:34 +0100

    nasty debug log: use timestamp format with more precision

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -15,6 +15,6 @@ FileUtils.mkdir_p "reports"
 DEBUG_FILE = File.open("reports/debug.out", "w+")
 
 AfterStep do |result, test_step|
-  DEBUG_FILE.write "#{Time.now.getutc}: #{test_step.location}\n"
+  DEBUG_FILE.write "#{Time.now.utc.round(10).iso8601(6)}: #{test_step.location}\n"
 end
 
