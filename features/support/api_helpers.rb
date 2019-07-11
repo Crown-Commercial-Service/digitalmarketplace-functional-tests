981ecbe8074859a60c5c7a084d08021964bd6585
Jonathan <jonathandrewkerr@gmail.com>
2019-07-11 14:33:01 +0100

    Update create_supplier (properly, this time)
    This will serve as a lesson for me to be more careful when changing Ruby code. The code I thought randomly generated a ten-digit number actually generated a number in the range 0-9999999999. Since we weren't validating DUNS number length, nobody noticed when a random supplier's DUNS number was, say, 3. I've replaced this with idiomatic ruby code that generates a 9-digit number. We are reasonably confident that this is kept as a string as it's passed around the system and therefore leading zeroes are a-okay.

--- features/support/api_helpers.rb
+++ features/support/api_helpers.rb
@@ -259,7 +259,7 @@ def create_supplier(custom_supplier_data = {})
   random_string = SecureRandom.hex
   supplier_data = {
     name: 'functional test supplier ' + random_string,
-    dunsNumber: rand(999999999).to_s,
+    dunsNumber: 9.times.map { rand(10) }.join,
     contactInformation: [{
       contactName: random_string,
       email: random_string + "-supplier@example.com",
