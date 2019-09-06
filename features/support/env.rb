ad8b5d53dbea7621189c95365c20f9084422d5bf
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-09-06 14:56:23 +0100

    Use Capybara's built-in driver for headless Firefox

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,14 +9,8 @@ RSpec.configure do |config|
 end
 
 if (ENV['BROWSER'] == 'firefox')
-  require 'selenium-webdriver'
+  require 'webdrivers/geckodriver'
   Capybara.default_driver = :selenium
-
-  Capybara.register_driver :selenium do |app|
-    http_client = Selenium::WebDriver::Remote::Http::Default.new
-    http_client.timeout = 180
-    Capybara::Selenium::Driver.new(app, browser: :firefox, http_client: http_client)
-  end
 else
   require 'webdrivers/chromedriver'
   Capybara.default_driver = :selenium_chrome_headless
