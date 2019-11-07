4cefc9bd236890122156b6f08ef4cf6a90ba4999
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-11-07 11:30:13 +0000

    add hacky AfterStep hook to take screenshots of each step matching certain criteria

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -15,6 +15,14 @@ FileUtils.mkdir_p "reports"
 DEBUG_FILE = File.open("reports/debug.#{Process.pid}.out", "a")
 
 AfterStep do |result, test_step|
-  DEBUG_FILE.write "#{Time.now.utc.round(10).iso8601(6)}: #{test_step.location}\n"
+  timestr = Time.now.utc.round(10).iso8601(6)
+  DEBUG_FILE.write "#{timestr}: #{test_step.location}: #{current_url}\n"
+  cp = URI(current_url).path
+  # buyer FE
+  # if cp.start_with? "/buyers/direct-award" or not cp.start_with?("/admin", "/suppliers", "/buyers", "/user")
+  # supplier FE
+  if cp.start_with? "/suppliers"
+    page.save_screenshot("walkthrough/#{timestr}.png", full: true)
+  end
 end
 
