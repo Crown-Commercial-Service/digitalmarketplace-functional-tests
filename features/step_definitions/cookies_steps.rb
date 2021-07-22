3637632ab08cde95c433d01c0b9186683e02d8bf
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2021-07-22 11:10:39 +0100

    fix linter errors

--- features/step_definitions/cookies_steps.rb
+++ features/step_definitions/cookies_steps.rb
@@ -11,34 +11,33 @@ end
 # Based on http://jbusser.github.io/2014/11/01/integration-testing-google-analytics-with-capybara-and-rspec.html
 
 def inline_http_requests
-
-    if ENV['CHROME']
-      # Chrome does not support network_traffic, instead we can extract this from the performance logs
-      logs = page.driver.browser.manage.logs.get(:performance)
-      # Store messages in a structure which is easier to work with
-      messages_array = logs.each_with_object([]) do |entry, messages|
-        message = JSON.parse(entry.message)
-        timestamp = entry.timestamp
-        messages << message
-        message.store(:timestamp, timestamp)
-      end
-      # Filter to only messages after test has started and requests with headers
-      messages_after_test_start = messages_array.select{|m| m[:timestamp] > @timestamp}
-      messages_after_test_start.map {|l| l.dig('message', 'params', 'headers')}.compact
-    else
-      page.driver.network_traffic.map do |traffic|
-      # Return all HTTP requests made by Poltergeist
+  if ENV['CHROME']
+    # Chrome does not support network_traffic, instead we can extract this from the performance logs
+    logs = page.driver.browser.manage.logs.get(:performance)
+    # Store messages in a structure which is easier to work with
+    messages_array = logs.each_with_object([]) do |entry, messages|
+      message = JSON.parse(entry.message)
+      timestamp = entry.timestamp
+      messages << message
+      message.store(:timestamp, timestamp)
+    end
+    # Filter to only messages after test has started and requests with headers
+    messages_after_test_start = messages_array.select { |m| m[:timestamp] > @timestamp }
+    messages_after_test_start.map { |l| l.dig('message', 'params', 'headers') }.compact
+  else
+    page.driver.network_traffic.map do |traffic|
+    # Return all HTTP requests made by Poltergeist
       URI.parse traffic.url
     end
   end
 end
 
 def google_analytics_requests
-    if ENV['CHROME']
-      inline_http_requests.select {|l| l.dig(":authority") == 'www.google-analytics.com'}
-    else
-      inline_http_requests.select do |request|
-      # Return all requests matching this host
+  if ENV['CHROME']
+    inline_http_requests.select { |l| l.dig(":authority") == 'www.google-analytics.com' }
+  else
+    inline_http_requests.select do |request|
+    # Return all requests matching this host
       request.host == 'www.google-analytics.com'
     end
   end
@@ -46,8 +45,8 @@ end
 
 def google_analytics_request_with_param(param)
   if ENV['CHROME']
-    collect_requests = google_analytics_requests{|r|  r[':path'].include? '/collect'}
-    collect_requests.find{|cq| cq[':path'].match param}
+    collect_requests = google_analytics_requests { |r|  r[':path'].include? '/collect' }
+    collect_requests.find { |cq| cq[':path'].match param }
   else
     google_analytics_requests.detect do |request|
     # We're only interested in analytics requests with `/collect/` in the path
